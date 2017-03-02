b9932dd95ab227de5b849b93dcd953abb249e803
icarus3421 <icarus3421@gmail.com>
2017-03-01 19:19:17 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,64 +1,31 @@
-print("Hello, World!")
+print("Hello World")
 
-#the point of this is to use a headless browser and/or to requests a series of documents
-#lets start with a facebook login
+f=open('0.html')
+inp=f.read()
+f.close()
 
-#IMPORTS
-import requests
-import time
+print(len(inp))
 
-#INIT SESSION
-print(dir(requests))
-c = requests.Session()
-headers = {"user_agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+import bs4
 
-def writetofile(dat, num):
-	print(dat)
-	f=open("out" + str(num) + ".html", "w+")
-	f.write(dat.content)
-	f.close()
+soup = bs4.BeautifulSoup(inp, "html.parser")
 
-#OPEN FACEBOOK
-dat = c.get("https://www.facebook.com", headers = headers)
-writetofile(dat, 1)
+#print(soup.prettify())
 
-#LOGIN
-email = r"joshuajolly3@gmail.com"
-pass_ = "Minecraft01"
+import json
+p = json.loads(inp)
 
-data = {"email":email,"pass":pass_}
+print("\nFirst Message:")
 
-dat = c.post("https://www.facebook.com/login.php?login_attempt=1&lwv=110", headers = headers, data = data)
-writetofile(dat, 2)
+print(p['actions'][0]['message_id'])
+print(p['actions'][0]['timestamp_datetime'])
+print(p['actions'][0]['author'])
+print(p['actions'][0]['body'])
 
-#GET MY PAGE
-time.sleep(1)
-dat = c.get("https://www.facebook.com/joshua.jolly.1238", headers = headers)
-writetofile(dat, 3)
+print("\nLast Message:")
 
-#GET TARGET'S PAGE
-time.sleep(2)
-dat = c.get("https://www.facebook.com/juanita.sprowell", headers = headers)
-writetofile(dat, 4)
-
-#GET MESSAGES
-time.sleep(1)
-dat = c.get("https://www.facebook.com/messages/t/100000176732853", headers = headers)
-writetofile(dat, 5)
-
-#GET OTHER MESSAGES
-user_id = 100000176732853
-
-for i in range(100):
-	data = """messages[user_ids][100000176732853][offset]={0}
-		&messages[user_ids][100000176732853][limit]=100000
-		&messages[user_ids][100000176732853][timestamp]
-		&client=mercury&__user=100008183341012&__a=1
-		&__dyn=7AzkXh8OAcjxd2u6aOGeFxqewRAKGgS8zAS-C2u6oqwWhE98nwgUaqG2yaBxebkwy6UnGi7VXDG4XzErz8iGt0TyKum4UpxSLGqu58nVUkzaxbxm1iyECcBxi48hxGbwYxyq-5pZ1G6XDxx4yplzEly8myEbt5xq498lBVpEcm8ypUhKHw
-		&__af=iw&__req=17&__be=-1&__pc=PHASED%3ADEFAULT&__rev=2865362
-		&fb_dtsg=AQFbG2C9c977%3AAQEmJLBD7pEk&ttstamp=265817098715067579957555558658169109747666685511269107""".format(i * 100000)
-	dat = c.post("https://www.facebook.com/ajax/mercury/thread_info.php?dpr=1", headers = headers, data = data)
-	f=open("data\\" + str(i) + ".html", "w+")
-	f.write(dat.content)
-	f.close()
-	break
+num = -1
+print(p['actions'][num]['message_id'])
+print(p['actions'][num]['timestamp_datetime'])
+print(p['actions'][num]['author'])
+print(p['actions'][num]['body'])
